0bbda5a32cd0a418af174d9ffc08749cf150305b
Derek Schatz <derek.schatz@sungardas.com>
2017-06-27 23:16:34 -0400

    added pair evaluator

--- PokerHands/PokerHands/Dealer.cs
+++ PokerHands/PokerHands/Dealer.cs
@@ -6,14 +6,12 @@ namespace PokerHands
     {
         public static WinningHand DetermineWinner(HandValue hand1, HandValue hand2)
         {
-            var handOfPlayer1 = hand1.HandType;
-            var handOfPlayer2 = hand2.HandType;
-            if (handOfPlayer1 == HandType.HighCard && handOfPlayer2 == HandType.HighCard)
+            if (hand1.HandType == HandType.HighCard && hand2.HandType == HandType.HighCard)
             {
                 for(int i = 4; i >= 0; i--)
                 {
                     var val1 = NumberMapper.NumberMapping[hand1.Cards[i].Value];
-                    var val2 = NumberMapper.NumberMapping[hand2.Cards[i].Value];
+                    var val2 = NumberMapper.NumberMapping[hand2.Cards[i].Value]; 
 
                     if (val1 > val2)
                     {
@@ -25,18 +23,41 @@ namespace PokerHands
                     }
                 }
             }
-            if(handOfPlayer1 == HandType.Pair && handOfPlayer2 == HandType.HighCard)
+            if(hand1.HandType == HandType.Pair && hand2.HandType == HandType.HighCard)
             {
                 return new WinningHand(hand1, Winner.Player1);
             }
-            if(handOfPlayer2 == HandType.Pair && handOfPlayer1 == HandType.HighCard)
+            if(hand2.HandType == HandType.Pair && hand1.HandType == HandType.HighCard)
             {
                 return new WinningHand(hand2, Winner.Player2);
             }
 
-            if(handOfPlayer1 == HandType.Pair && handOfPlayer2 == HandType.Pair)
+            string[] unPairedCards = new string[3];
+            
+            if(hand1.HandType == HandType.Pair && hand2.HandType == HandType.Pair)
             {
-                
+                for(int i = 4; i >= 0; i--)
+                {
+                    //if numbers are equal return the value of the number 
+                    if(hand1.Cards[i].Value == hand1.Cards[i - 1].Value)
+                    {
+                        var pairVal1 = NumberMapper.NumberMapping[hand1.Cards[i].Value];
+                    }
+                    if(hand1.Cards[i].Value != hand1.Cards[i - 1].Value)
+                    {
+                        for(int x = 0; x < 3; x++)
+                        {
+                            //add the unpaired cards into a new array
+                            unPairedCards[x] = hand1.Cards[i].Value;
+                            
+                            
+
+                        }
+                     
+                    }
+                }
+
+               
             }
             return WinningHand.Draw;
         }
